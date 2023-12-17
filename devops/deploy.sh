28af1f9ea804e259ad3a596a7e30a560867fcb55
Alexander3006 <ukrpresident2036@gmail.com>
2023-12-17 19:45:42 +0200

    fix: deploy

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+kubectl create namespace logistician-app
+
 kubectl apply -f ./postgres/postgres.deployment.yaml
 kubectl apply -f ./postgres/postgres.service.yaml
 
 kubectl apply -f ./redis/redis.deployment.yaml
 kubectl apply -f ./redis/redis.service.yaml
 
+kubectl apply -f ./rabbitmq/rabbitmq.deployment.yaml
+kubectl apply -f ./rabbitmq/rabbitmq.service.yaml
+
+kubectl apply -f ./zipkin/zipkin.deployment.yaml
+kubectl apply -f ./zipkin/zipkin.service.yaml
+
 kubectl apply -f ./logistician/logistician.config-map.yaml
 kubectl apply -f ./logistician/logistician.deployment.yaml
 kubectl apply -f ./logistician/logistician.service.yaml
 kubectl apply -f ./logistician/logistician.ingress.yaml
+
+# minikube tunnel
